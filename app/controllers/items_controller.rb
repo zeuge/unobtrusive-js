4cfeee52237cdda38c0eb188708cab650c2ebff1
Eugene Zhukov <zeuge74@gmail.com>
2015-02-14 20:29:55 +0500

    edit: index and create items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,22 @@
 class ItemsController < ApplicationController
+  
   def index
+    @items = Item.all
+    @item = Item.new    
+  end
+
+  def create
+    @item = Item.new(items_params)
+    if @item.save
+      flash[:notice] = "Successfully created item."
+      @items = Item.all
+    else
+      flash[:alert] = "Error"
+    end
+  end
+
+private
+  def items_params
+    params.require(:item).permit(:name, :description)
   end
 end
