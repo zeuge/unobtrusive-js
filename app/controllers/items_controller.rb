ac1fadd7ac94e57a9b46ea01e31c3afdefd8c2dc
Eugene Zhukov <zeuge74@gmail.com>
2015-02-16 20:14:04 +0500

    add: action edit & destroy

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,21 +1,44 @@
 class ItemsController < ApplicationController
-  
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+
   def index
+    @item = Item.new
     @items = Item.all
-    @item = Item.new    
   end
 
   def create
-    @item = Item.new(items_params)
+    @item = Item.new(item_params)
     if @item.save
-      flash[:notice] = "Successfully created item."
+      flash[:notice] = "Item was successfully created."
       @items = Item.all
     end
   end
 
+  def edit
+
+  end
+
+  def update
+    if @item.update(item_params)
+      flash[:notice] = "Item was successfully updated."
+      @item = Item.new
+      @items = Item.all
+    end
+  end
+
+  def destroy
+    @item.destroy
+    flash[:notice] = "Item was successfully destroyed."
+    @items = Item.all
+  end
+
 private
-  def items_params
+  def item_params
     params.require(:item).permit(:name, :description)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
   
 end
